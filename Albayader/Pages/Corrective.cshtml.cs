e6e33015e4ecb8c4850eea1922a393f2ee580165
bassam Mohaisen <mohaisen_bassam@hotmail.com>
2022-05-29 12:58:09 +0300

    profile

---
+++ Albayader/Pages/Corrective.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AlbayaderWeb.Pages
+{
+    public class CorrectiveModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Albayader/Pages/Shared/Calendar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AlbayaderWeb.Pages.Shared
+{
+    public class CalenderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
