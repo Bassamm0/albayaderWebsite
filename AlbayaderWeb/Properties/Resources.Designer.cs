09cb49865a88c2d980c4d4537bb951b8d6dd2b43
bassam Mohaisen <mohaisen_bassam@hotmail.com>
2023-03-06 16:27:52 +0400

    c

--- AlbayaderWeb/Properties/Resources.Designer.cs
+++ AlbayaderWeb/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AlbayaderWeb.Properties {
+namespace Albayaderweb.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace AlbayaderWeb.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AlbayaderWeb.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Albayaderweb.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
